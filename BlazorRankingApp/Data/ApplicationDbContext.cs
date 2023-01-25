04f169b0bf7cbcc19d22da2c4fe3b0945d735e32
Inoh Park <inohzzang@gmail.com>
2023-01-25 14:47:56 +0900

    DB attachment

---
+++ BlazorRankingApp/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using BlazorRankingApp.Data.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BlazorRankingApp.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public DbSet<GameResult> GameResults { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
