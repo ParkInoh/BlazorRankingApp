91b1e0f2b7e6398b5e3b91c91231be471adf13c7
Inoh Park <inohzzang@gmail.com>
2023-02-02 13:49:52 +0900

    Web API 추가
    CRUD 방식으로 구현

---
+++ WebAPI/Controllers/RankingController.cs
@@ -0,0 +1,94 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SharedData.Models;
+using WebAPI.Data;
+
+namespace WebAPI.Controllers
+{
+    // ApiController의 특징: C# 객체를 반환해도 된다.
+    // null: 204 No Context
+    // string: text/plain
+    // 그 외: application/json
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RankingController : ControllerBase
+    {
+        ApplicationDbContext _context;
+
+        public RankingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // Create
+        // FromBody: body에서 가져온 것임을 알림
+        [HttpPost]
+        public GameResult AddGameResult([FromBody] GameResult gameResult)
+        {
+            _context.GameResults.Add(gameResult);
+            _context.SaveChanges();
+
+            return gameResult;
+        }
+
+
+        // Read
+        [HttpGet]
+        public List<GameResult> GetGameResults()
+        {
+            List<GameResult> results = _context.GameResults
+                .OrderByDescending(item => item.Score)
+                .ToList();
+
+            return results;
+        }
+
+        // 요구하는 추가 정보를 주어야 함
+        [HttpGet("{id}")]
+        public GameResult? GetGameResult(int id)
+        {
+            GameResult? result = _context.GameResults
+                .Where(item => item.Id == id)
+                .FirstOrDefault();
+
+            return result;
+        }
+
+        // Update
+        [HttpPut]
+        public bool UpdateGameResult([FromBody] GameResult gameResult)
+        {
+            var findResult = _context.GameResults
+                .Where(x => x.Id == gameResult.Id)
+                .FirstOrDefault();
+
+            // 대상이 없음
+            if (findResult == null)
+                return false;
+
+            findResult.UserName = gameResult.UserName;
+            findResult.Score = gameResult.Score;
+            _context.SaveChanges();
+
+            return true;
+        }
+
+        // Delete
+        [HttpDelete("{id}")]
+        public bool DeleteGameResult(int id)
+        {
+            var findResult = _context.GameResults
+                .Where(x => x.Id == id)
+                .FirstOrDefault();
+
+            // 대상이 없음
+            if (findResult == null)
+                return false;
+
+            _context.GameResults.Remove(findResult);
+            _context.SaveChanges();
+            return true;
+        }
+
+    }
+}
